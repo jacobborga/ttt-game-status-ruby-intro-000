9db2985b7ab7af0d1199f2a0bf3e939a1df284fe
Jacob Borga <jdborga@gmail.com>
2021-03-04 16:10:05 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,9 +5,9 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0, 1, 2], # <
+  [0, 1, 2], # <---- Rows
   [3, 4, 5], # <
-  [6, 7, 8], # <---- Rows
+  [6, 7, 8], # <
   [0, 3, 6], # <---- Columns
   [1, 4, 7], # <
   [2, 5, 8], # <
