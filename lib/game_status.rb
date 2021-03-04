2cef93758f462b3d4df9d1060730f0d4964d2467
Jacob Borga <jdborga@gmail.com>
2021-03-04 16:38:23 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -40,7 +40,7 @@ def full?(board)
 end
 
 def draw?(board)
-  if full?(board) && won?(board)
+  if (full?(board) && won?(board)) || (!full?(board) && !won?(board))
     return false
   end
   return true
